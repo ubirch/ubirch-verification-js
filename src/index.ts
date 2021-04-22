b32d1f620a947de8d60af4a5b3ff22eb5f25b3ef
afternoon2 <poprostuantolak@gmail.com>
2021-04-22 09:22:05 +0200

    Shorten models export

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,4 @@
 import UbirchVerification from './verification';
-import {
-  EError,
-  EHashAlgorithms,
-  EInfo,
-  ELanguages,
-  EStages,
-  EUppStates,
-  EVerificationState,
-  IUbirchBlockchain,
-  IUbirchBlockchainAnchor,
-  IUbirchBlockchainAnchorProperties,
-  IUbirchBlockchainAnchorRAW,
-  IUbirchError,
-  IUbirchInfo,
-  IUbirchUpp,
-  IUbirchVerificationConfig,
-  IUbirchVerificationResponse,
-  IUbirchVerificationResult,
-} from './models/models';
+import * as models from './models/models';
 
-export { UbirchVerification };
-export {
-  EError,
-  EHashAlgorithms,
-  EInfo,
-  ELanguages,
-  EStages,
-  EUppStates,
-  EVerificationState,
-  IUbirchBlockchain,
-  IUbirchBlockchainAnchor,
-  IUbirchBlockchainAnchorProperties,
-  IUbirchBlockchainAnchorRAW,
-  IUbirchError,
-  IUbirchInfo,
-  IUbirchUpp,
-  IUbirchVerificationConfig,
-  IUbirchVerificationResponse,
-  IUbirchVerificationResult,
-};
+export { UbirchVerification, models };
