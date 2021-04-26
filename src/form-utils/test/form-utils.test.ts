b7477a8ba40a5c774ccec505e79b940291e612e6
Tomasz Krawczyk <grubach1@gmail.com>
2021-04-26 14:29:15 +0200

    separator tests

--- src/form-utils/test/form-utils.test.ts
+++ src/form-utils/test/form-utils.test.ts
@@ -4,23 +4,28 @@ beforeEach(() => {});
 
 describe('Get params from URL', () => {
   test('should get params', () => {
-    global.window.history.pushState({}, '', '?a=1&b=2');
-    const result = FormUtils.getFormParamsFromUrl(global.window);
+    global.window.history.pushState({}, '', '?a=1;b=2');
+    const result = FormUtils.getFormParamsFromUrl(global.window, ';');
     expect(result).toEqual({ a: '1', b: '2' });
   });
   test('should get param', () => {
     global.window.history.pushState({}, '', '?c=3');
-    const result = FormUtils.getFormParamsFromUrl(global.window);
+    const result = FormUtils.getFormParamsFromUrl(global.window, ';');
     expect(result).toEqual({ c: '3' });
   });
   test('should get params with array', () => {
-    global.window.history.pushState({}, '', '?d=4&e=5,6,7');
-    const result = FormUtils.getFormParamsFromUrl(global.window);
+    global.window.history.pushState({}, '', '?d=4;e=5,6,7');
+    const result = FormUtils.getFormParamsFromUrl(global.window, ';');
+    expect(result).toEqual({ d: '4', e: ['5', '6', '7'] });
+  });
+  test('should get params started with hash and divided by ampersand', () => {
+    global.window.history.pushState({}, '', '#d=4&e=5,6,7');
+    const result = FormUtils.getFormParamsFromUrl(global.window, '&');
     expect(result).toEqual({ d: '4', e: ['5', '6', '7'] });
   });
   test("shouldn't get params", () => {
     global.window.history.pushState({}, '', '?');
-    const result = FormUtils.getFormParamsFromUrl(global.window);
+    const result = FormUtils.getFormParamsFromUrl(global.window, ';');
     expect(result).toEqual({});
   });
 });
