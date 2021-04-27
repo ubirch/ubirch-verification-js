fbc9e06f0f82984f1a9730ccea9547e41f64c29c
Tomasz Krawczyk <grubach1@gmail.com>
2021-04-27 09:18:46 +0200

    export form utils in index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import UbirchVerification from './verification';
+import FormUtils from './form-utils';
 import * as models from './models/models';
 
-export { UbirchVerification, models };
+export { UbirchVerification, FormUtils, models };
