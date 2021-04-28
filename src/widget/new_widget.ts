3712e992608843840ec1e98bdd06a6bc4f34d526
afternoon2 <poprostuantolak@gmail.com>
2021-04-28 10:19:34 +0200

    WIP: display verification results

--- src/widget/new_widget.ts
+++ src/widget/new_widget.ts
@@ -1,11 +1,16 @@
-import { Observable } from "rxjs";
-import { EError, ELanguages, IUbirchMessage, IUbirchVerificationResult } from "../models/models";
-import { initTranslation } from "../utils/i18n";
+import {
+  EError,
+  ELanguages,
+  EVerificationState,
+  IUbirchVerificationResult,
+} from '../models/models';
+import { initTranslations } from '../utils/translations';
 import * as de from '../assets/i18n/widget/de.json';
 import * as en from '../assets/i18n/widget/en.json';
 import environment from '../environment';
+import i18next from 'i18next';
 
-initTranslation({ en, de })
+initTranslations({ en, de });
 
 export enum WidgetClassNameSuffixes {
   InfoText = 'info-text',
@@ -19,36 +24,62 @@ export interface IUbirchVerificationWidgetConfig {
   language?: ELanguages;
   openConsoleInSameTarget?: boolean;
   initialVerificationResult?: IUbirchVerificationResult;
-  infoWatcher?: Observable<IUbirchMessage>;
 }
 
 export class UbirchVerificationWidget {
   private host: HTMLElement;
-  private sealInfoText: HTMLElement;
-  private sealOutput: HTMLElement;
-  private resultOutput: HTMLElement;
-  private errorOutput: HTMLElement;
+  private verificationResult: IUbirchVerificationResult;
 
   constructor(config: IUbirchVerificationWidgetConfig) {
     const host = document.querySelector(config.hostSelector);
     if (!host) throw new Error(EError.ELEMENT_FOR_WIDGET_SELECTOR_NOT_FOUND);
     this.host = host as HTMLElement;
-    this.init();
+    this.update(config.initialVerificationResult);
   }
 
-  private init(): void {
-    this.host.insertAdjacentHTML('beforeend', this.template);
-    this.sealOutput = this.host.querySelector('.ubirch');
-    this.sealInfoText = this.host.querySelector(WidgetClassNameSuffixes.InfoText);
-    this.sealOutput = this.host.querySelector(WidgetClassNameSuffixes.SealOutput);
-    this.resultOutput = this.host.querySelector(WidgetClassNameSuffixes.ResultOutput);
-    this.errorOutput = this.host.querySelector(WidgetClassNameSuffixes.ErrorOutput);
+  public update(verificationResult?: IUbirchVerificationResult) {
+    this.verificationResult = verificationResult;
+    this.renderTemplate();
   }
 
-  private get template(): string {
-    return Object.values(WidgetClassNameSuffixes).reduce((template: string, val: string) => {
-      template += `<div class="ubirch-${val}"></div>`;
-      return template;
-    }, '');
+  private renderTemplate(): void {
+    this.host.insertAdjacentHTML(
+      'beforeend',
+      `<div class="ubirch-${WidgetClassNameSuffixes.InfoText}">${this.getHeadlineInfoText()}</div>
+        <div class="ubirch-${WidgetClassNameSuffixes.SealOutput}"></div>
+        <div class="ubirch-${WidgetClassNameSuffixes.ResultOutput}"></div>
+        <div class="ubirch-${WidgetClassNameSuffixes.ErrorOutput}">${this.getErrorOutput()}</div>`
+    );
+  }
+
+  private getErrorOutput(): string {
+    if (this.verificationResult && this.verificationResult.verificationState === EVerificationState.VERIFICATION_FAILED) {
+      return `<span class="ubirch-error-output">Error msg TBD</span>`;
+    }
+    return '';
+  }
+
+  private getHeadlineInfoText(): string {
+    let classNames = 'ubirch-verification-info';
+    let tKey = 'PENDING.info';
+    if (this.verificationResult) {
+      switch (this.verificationResult.verificationState) {
+        case EVerificationState.VERIFICATION_SUCCESSFUL:
+        case EVerificationState.VERIFICATION_PARTLY_SUCCESSFUL:
+          classNames = 'ubirch-verification-success ubirch-verification-headline';
+          tKey = 'SUCCESS.headline';
+          break;
+        case EVerificationState.VERIFICATION_FAILED:
+          classNames = 'ubirch-verification-fail';
+          tKey = 'FAIL.info';
+          break;
+        case EVerificationState.VERIFICATION_PENDING:
+        default:
+          classNames = 'ubirch-verification-info';
+          tKey = 'PENDING.info';
+          break;
+      }
+    }
+    return `<span class="${classNames}">${i18next.t(tKey)}</span>`;
   }
 }
--- src/widget/test/widget.test.ts
+++ src/widget/test/widget.test.ts
@@ -1,10 +1,10 @@
-import UbirchVerificationWidget from '../';
+import { UbirchVerificationWidget } from '../new_widget';
 import { WidgetClassNameSuffixes } from '../widget';
 
 describe('Widget', () => {
   test('If widget mounts in the host element', () => {
     const root = document.body;
-    new UbirchVerificationWidget({ elementSelector: 'body' });
+    new UbirchVerificationWidget({ hostSelector: 'body' });
 
     Object.values(WidgetClassNameSuffixes).forEach((suffix) => {
       expect(root.querySelector(`.ubirch-${suffix}`)).not.toBe(null);
