871ede5061aeeb9e55b0b9d317c1a8666568fcab
afternoon2 <poprostuantolak@gmail.com>
2021-04-30 16:17:17 +0200

    Add showSeal method

--- src/widget/new_widget.ts
+++ src/widget/new_widget.ts
@@ -1,3 +1,4 @@
+import i18next from 'i18next';
 import { Observable } from 'rxjs';
 import classnames from 'classnames';
 import {
@@ -5,13 +6,15 @@ import {
   ELanguages,
   EMessageType,
   EVerificationState,
+  IUbirchBlockchain,
+  IUbirchVerificationAnchorProperties,
   UbirchMessage,
 } from '../models/models';
 import { initTranslations } from '../utils/translations';
 import * as de from '../assets/i18n/widget/de.json';
 import * as en from '../assets/i18n/widget/en.json';
-// import environment from '../environment';
-import i18next from 'i18next';
+import environment from '../environment';
+import * as BlockchainSettings from '../blockchain-assets/blockchain-settings.json';
 import styles from './widget.module.scss';
 
 initTranslations({ en, de });
@@ -24,11 +27,13 @@ export interface IUbirchVerificationWidgetConfig {
 
 export class UbirchVerificationWidget {
   private host: HTMLElement;
+  private openConsoleInSameTarget: boolean;
 
   constructor(config: IUbirchVerificationWidgetConfig) {
     const host = document.querySelector(config.hostSelector);
     if (!host) throw new Error(EError.ELEMENT_FOR_WIDGET_SELECTOR_NOT_FOUND);
     this.host = host as HTMLElement;
+    this.openConsoleInSameTarget = config.openConsoleInSameTarget || false;
     config.messenger.subscribe((message) => {
       if (message) this.render(message);
     });
@@ -101,5 +106,29 @@ export class UbirchVerificationWidget {
       default:
         return '';
     }
+  }
+
+  private showSeal(successful: boolean, hash: string, noLink: boolean = false) {
+    const iconSrcSuffix: string = successful
+      ? BlockchainSettings.ubirchIcons.seal
+      : BlockchainSettings.ubirchIcons.no_seal;
+    const iconId = `ubirch-verification-${successful ? 'seal' : 'no-seal'}-img`;
+    const icon = this.createIconString(`${environment.assets_url_prefix}${iconSrcSuffix}`, iconId);
+
+    if (noLink) {
+      return icon;
+    } else {
+      const encodedHash: string = encodeURIComponent(hash);
+      const href = `${environment.console_verify_url}?hash=${encodedHash}`;
+      return `
+        <a href="${href}" ${!this.openConsoleInSameTarget ? 'target="_blank' : ''}>
+          ${icon}
+        </a>
+      `;
+    }
+  }
+
+  private createIconString(src: string, id: string, width?: string, height?: string): string {
+    return `<img src="${src}" id="${id}" style="width: ${width}; height: ${height}" />`;
   }
 }
