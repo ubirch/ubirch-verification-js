83fdb439f337ed8cd1c30dafdef6b3bb63ebfc45
afternoon2 <poprostuantolak@gmail.com>
2021-05-21 12:06:37 +0200

    fix tests

--- src/widget/test/widget.test.ts
+++ src/widget/test/widget.test.ts
@@ -678,7 +678,7 @@ describe('Widget', () => {
         const sealIconEl = root.querySelector('#ubirch-verification-widget-seal-output');
 
         expect(sealIconEl.children.length).toBeGreaterThan(0);
-        expect(sealIconEl.querySelector('a')).toBe(null);
+        expect(sealIconEl.querySelector('a')).not.toBe(null);
       });
     });
   });
